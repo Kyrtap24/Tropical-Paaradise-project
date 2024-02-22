26cd14c2ff55b43296196e5f99759b1e7c18969a
Kyrtap24 <patryktupacz@gmail.com>
2024-02-22 17:20:58 +0100

    fixed nav

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
-const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('.section')
-const footerYear = document.querySelector('.footer__year')
-
-const handleNav = () => {
-	nav.classList.toggle('nav--active')
-
-	navBtnBars.classList.remove('black-bars-color')
-
-	allNavItems.forEach(item => {const nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item')
 const navBtnBars = document.querySelector('.burger-btn__bars')
@@ -65,44 +53,4 @@ const handleCurrentYear = () => {
 
 handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
-window.addEventListener('scroll', handleObserver)
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
-		})
-	})
-}
-
-const handleNavItemsAnimation = () => {
-	let delayTime = 0
-
-	allNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
-}
-handleNavItemsAnimation()
-
-const handleObserver = () => {
-	const currentSection = window.scrollY
-
-	allSections.forEach(section => {
-		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-			navBtnBars.classList.add('black-bars-color')
-		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-			navBtnBars.classList.remove('black-bars-color')
-		}
-	})
-}
-
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear()
-	footerYear.innerText = year
-}
-
-handleCurrentYear()
-
-navBtn.addEventListener('click', handleNav)
-navBtn.addEventListener('click', handleNavItemsAnimation)
 window.addEventListener('scroll', handleObserver)
